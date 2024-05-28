3c242db7b00b93612125db900a215a5872599869
Taseen <taseen@sofy.ai>
2024-05-28 23:58:36 +1000

    push

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,3 +1,14 @@
 -- Grant all privileges to 'admin' user from any host within the network
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' IDENTIFIED BY 'admin';
 FLUSH PRIVILEGES;
+
+USE product_reviews;
+
+CREATE TABLE reviews (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_review TEXT,
+    product_class VARCHAR(255),
+    product_department VARCHAR(255),
+    user_age INT,
+    tag VARCHAR(50)
+);
